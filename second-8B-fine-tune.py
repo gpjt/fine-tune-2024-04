5635c2eef5646d4ffd1a795aa233890dedd81628
Giles Thomas <giles@giles.net>
2024-05-18 00:11:56 +0100

    Missing files

---
+++ second-8B-fine-tune.py
@@ -0,0 +1,46 @@
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import TrainingArguments, Trainer
+
+
+dataset_source = "timdettmers/openassistant-guanaco"
+dataset = load_dataset(dataset_source)
+
+base_model = "meta-llama/Meta-Llama-3-8B"
+
+tokenizer = AutoTokenizer.from_pretrained(base_model)
+tokenizer.pad_token = tokenizer.eos_token
+model = AutoModelForCausalLM.from_pretrained(base_model)
+
+batch_size = 1
+args = TrainingArguments(
+    'outputs',
+    learning_rate=8e-5,
+    warmup_ratio=0.1,
+    lr_scheduler_type='cosine',
+    fp16=True,
+    evaluation_strategy="epoch",
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size * 2,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    deepspeed="ds_config.json",
+    report_to='none',
+)
+
+
+def tokenize_function(examples):
+    tokenized = tokenizer(examples["text"], truncation=True, padding="max_length", max_length=2048)
+    tokenized["labels"] = tokenized["input_ids"][:]
+    return tokenized
+
+
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+
+trainer = Trainer(
+    model, args,
+    train_dataset=tokenized_dataset['train'],
+    eval_dataset=tokenized_dataset['test'],
+    tokenizer=tokenizer,
+)
+trainer.train()
