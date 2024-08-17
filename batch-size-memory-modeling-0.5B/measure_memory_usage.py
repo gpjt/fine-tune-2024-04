36cd9ce295ee40a6de64d87cbbc78ebfb6d953da
Giles Thomas <giles@giles.net>
2024-08-17 21:06:35 +0100

    Copy of 0.5B training code for batch size measurements

---
+++ batch-size-memory-modeling-0.5B/measure_memory_usage.py
@@ -0,0 +1,24 @@
+import subprocess
+
+with open("./results.csv", "w") as f:
+    pass
+
+for sequence_length in range(1, 2049):
+    succeeded = False
+    tries = 0
+    while not succeeded and tries < 5:
+        tries += 1
+        try:
+            subprocess.check_call([
+                "deepspeed",
+                "measure_memory_usage_for_sequence_length.py",
+                "--",
+                str(sequence_length)
+            ])
+            succeeded = True
+        except subprocess.CalledProcessError as exc:
+            print(f"************************** ERROR {exc}")
+
+    if not succeeded:
+        print("***************** Too many failures, crapping out")
+        break
